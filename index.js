21a5d0f169a93adcfe30756a51a2e395ece5d696
Joao casotti desktop <augusto_casotti@hotmail.com>
2025-08-20 05:44:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+const express = require("express");
+const hbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
+const PORT = process.env.PORT ;
+
+const app = express();
+
+const conn = require("./db/conn");
+
+// Models
+const Tought = require("./models/Tought");
+
+// routes
+const toughtsRoutes = require("./routes/ToughtRoutes");
+
+const ToughController = require("./controllers/ToughtController");
+
+app.engine("handlebars", hbs.engine());
+app.set("view engine", "handlebars");
+app.set('views', './views');
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+//session middleware
+app.use(
+  session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    },
+  }),
+)
+
+// flash messages
+app.use(flash());
+
+app.use(express.static("public"));
+
+// set session to res
+app.use((req, res, next) => {
+
+  next();
+});
+
+app.use("/toughts", toughtsRoutes);
+
+
+app.get("/", ToughController.showToughts);
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.log(`Error: ${err}`));
