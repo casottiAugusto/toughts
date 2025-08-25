74ef580f866472205919499db5b6afcc9f5463b1
Joao casotti desktop <augusto_casotti@hotmail.com>
2025-08-25 09:27:55 -0300

    validando quantidade de mensagems

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -19,8 +19,12 @@ module.exports = class ToughtController {
     }
 
     const toughts = user.Toughts.map((tought) => tought.dataValues);
+    let emptyToughts = false;
+    if (toughts.length === 0) {
+      emptyToughts = true;
+    }
 
-    res.render("toughts/dashboard", { toughts });
+    res.render("toughts/dashboard", { toughts, emptyToughts });
   }
   static createTought(req, res) {
     res.render("toughts/create");
@@ -34,6 +38,19 @@ module.exports = class ToughtController {
     try {
       await Tought.create(tought);
       req.flash("message", "Pensamento criado com sucesso!");
+      req.session.save(() => {
+        res.redirect("/toughts/dashboard");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  static async removeTought(req, res) {
+    const id = req.body.id;
+
+    try {
+      await Tought.destroy({ where: { id } });
+      req.flash("message", "Pensamento removido com sucesso!");
       req.session.save(() => {
         res.redirect("/toughts/dashboard");
       });
--- routes/ToughtRoutes.js
+++ routes/ToughtRoutes.js
@@ -7,6 +7,7 @@ const { checkAuth } = require("../helpers/auth");
 router.get("/", ToughtController.showToughts);
 router.get("/add", checkAuth, ToughtController.createTought);
 router.post("/add", checkAuth, ToughtController.createToughtSave);
+router.post("/remove", checkAuth, ToughtController.removeTought);
 
 router.get("/dashboard", checkAuth, ToughtController.dashboard);
 module.exports = router;
